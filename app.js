25b1382b12374a780d6685bf8737b634b6d22d66
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-02-15 12:00:59 +0330

    timer is cleaned

--- app.js
+++ app.js
@@ -25,33 +25,27 @@ container.onkeyup = (e) => {
 // ========================= Countdown Timer
 let t = 0,
   m,
-  s,
-  m_text,
-  s_text;
-startTimer();
-function startTimer() {
+  s;
+const startTimer = () => {
   if (t != 0) return;
   t = 120000;
-  document.getElementById("btn-resend").innerText = getTime();
-  document.getElementById("btn-resend").classList.add("disabled");
-  var x = setInterval(function () {
+  const resendField = document.querySelector("#resendField");
+  resendField.innerText = getTime();
+  resendField.classList.add("disabled");
+  var x = setInterval(() => {
     t -= 1000;
-    document.getElementById("btn-resend").innerText = getTime();
+    resendField.innerText = getTime();
     if (t == 0) {
       clearInterval(x);
-      document.getElementById("btn-resend").innerText =
-        "ارسال کد به صورت پیامک";
-      document.getElementById("btn-resend").classList.remove("disabled");
+      resendField.innerText = "ارسال کد به صورت پیامک";
+      resendField.classList.remove("disabled");
     }
   }, 1000);
-}
-function getTime() {
-  m = Math.floor(t / 60000);
-  s = (t % 60000) / 1000;
-  if (m < 10) m_text = m + " 0 ";
-  else m_text = m;
-  if (s < 10) s_text = s + " 0 ";
-  else s_text = s;
+};
+const getTime = () => {
+  m = Math.floor(t / 60000).toString();
+  s = ((t % 60000) / 1000).toString();
+  return `${s.padStart(2, "0")}  :  ${m.padStart(2, "0")}`;
+};
 
-  return s_text + " : " + m_text;
-}
+startTimer();
