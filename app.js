14eb159f91f6d4c1ebf43f5095678311b4fa76ab
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-02-16 12:31:01 +0330

    Edit Number Part

--- app.js
+++ app.js
@@ -27,24 +27,26 @@ const timeProgress = document.querySelector("#timeProgress");
 let t = 0,
   m,
   s;
+let timerInterval;
 const startTimer = () => {
-  if (t != 0) return;
-  t = 120000;
-  const resendField = document.querySelector("#resendField");
-  resendField.innerText = `${getTime()}  تا ارسال مجدد کد `;
-  resendField.classList.add("disabled");
-  timeProgress.max = t / 1000;
-  var x = setInterval(() => {
-    t -= 1000;
-    timeProgress.value = t / 1000;
+  if (t !== -1) {
+    t = 120000;
+    const resendField = document.querySelector("#resendField");
     resendField.innerText = `${getTime()}  تا ارسال مجدد کد `;
-    if (t == 0) {
-      clearInterval(x);
-      resendField.innerText = "ارسال کد به صورت پیامک";
-      resendField.classList.remove("disabled");
-      timeProgress.style.display = "none";
-    }
-  }, 1000);
+    resendField.classList.add("disabled");
+    timeProgress.max = t / 1000;
+    timerInterval = setInterval(() => {
+      t -= 1000;
+      timeProgress.value = t / 1000;
+      resendField.innerText = `${getTime()}  تا ارسال مجدد کد `;
+      if (t == 0) {
+        clearInterval(timerInterval);
+        resendField.innerText = "ارسال کد به صورت پیامک";
+        resendField.classList.remove("disabled");
+        timeProgress.style.display = "none";
+      }
+    }, 1000);
+  }
 };
 const getTime = () => {
   m = Math.floor(t / 60000).toString();
@@ -59,6 +61,7 @@ const cardGetNumberOrEmailBody = document.querySelector(
 );
 const NumberOrEmailForm = document.querySelector("#NumberOrEmailForm");
 const verifingNumber = document.querySelector(".verifingNumber");
+const btnEditNumber = document.querySelector(".btnEditNumber");
 
 cardSmsPasswordBody.style.display = "none";
 NumberOrEmailForm.addEventListener("submit", (e) => {
@@ -67,4 +70,9 @@ NumberOrEmailForm.addEventListener("submit", (e) => {
   verifingNumber.innerText = e.target.firstElementChild.value;
   cardGetNumberOrEmailBody.style.display = "none";
   cardSmsPasswordBody.style.display = "flex";
+});
+btnEditNumber.addEventListener("click", () => {
+  cardGetNumberOrEmailBody.style.display = "flex";
+  cardSmsPasswordBody.style.display = "none";
+  clearInterval(timerInterval);
 });
